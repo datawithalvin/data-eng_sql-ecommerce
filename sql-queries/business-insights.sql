4753dd6f439b05550991cbc1ae60f6ce288b00a7
Alpin Noza <alvinnoza.data@gmail.com>
2023-07-14 00:25:57 +0700

    update some queries

--- sql-queries/business-insights.sql
+++ sql-queries/business-insights.sql
@@ -1,155 +1,155 @@
 -- Displaying the first 20 rows from all_transactions table for initial data exploration
-select * 
-from all_transactions 
-limit 20;
+SELECT * 
+FROM all_transactions 
+LIMIT 20;
 
 -- ************************************************
 
 -- Query to find the top 10 products that generate the highest revenue. 
 -- This helps to understand which products contribute most to the company's income.
-select  
+SELECT  
 	hits_product_productsku, 
 	hits_product_v2productname, 
-	SUM(hits_product_productrevenue) as total_revenue
-from 
+	SUM(hits_product_productrevenue) AS total_revenue
+FROM 
 	all_transactions
-where 
-	hits_product_productsku is not null
-group by 
+WHERE 
+	hits_product_productsku IS NOT NULL
+GROUP BY 
 	hits_product_productsku, hits_product_v2productname
-order by
-	total_revenue desc
-limit 10;
+ORDER BY
+	total_revenue DESC
+LIMIT 10;
 
 -- ************************************************
 
 -- Query to find the top 10 cities that generate the highest number of transactions. 
 -- This gives an overview of the geographical distribution of the company's sales.
-select 
+SELECT 
 	geonetwork_city, 
-	COUNT(*) as number_of_transactions
-from
+	COUNT(*) AS number_of_transactions
+FROM
 	all_transactions
-where 
-	geonetwork_city is not null and geonetwork_city <> 'not available in demo dataset'
-group by 
+WHERE 
+	geonetwork_city IS NOT NULL AND geonetwork_city <> 'not available in demo dataset'
+GROUP BY 
 	geonetwork_city
-order by 
-	number_of_transactions desc 
-limit 10;
+ORDER BY 
+	number_of_transactions DESC 
+LIMIT 10;
 
 -- ************************************************
 
 -- Query to find the top 10 countries that generate the highest number of transactions. 
 -- This could inform international marketing strategies.
-select 
+SELECT 
 	geonetwork_country, 
-	COUNT(*) as number_of_transactions
-from
+	COUNT(*) AS number_of_transactions
+FROM
 	all_transactions
-where 
-	geonetwork_country is not null and geonetwork_country <> 'not available in demo dataset'
-group by 
+WHERE 
+	geonetwork_country IS NOT NULL AND geonetwork_country <> 'not available in demo dataset'
+GROUP BY 
 	geonetwork_country
-order by 
-	number_of_transactions desc 
-limit 10;
+ORDER BY 
+	number_of_transactions DESC 
+LIMIT 10;
 
 -- ************************************************
 
 -- Query to find the average time a user spends on the site. 
 -- This is a useful indicator of user engagement.
-select 
-	ROUND(AVG(totals_timeonsite), 2) as average_time_spend_seconds,
-	ROUND(AVG(totals_timeonsite) / 60, 2) as average_time_spend_minutes
-from
+SELECT 
+	ROUND(AVG(totals_timeonsite), 2) AS average_time_spend_seconds,
+	ROUND(AVG(totals_timeonsite) / 60, 2) AS average_time_spend_minutes
+FROM
 	all_transactions;
 
 -- ************************************************
 
 -- Query to find the top 10 products with the highest revenue per transaction. 
 -- This could help identify high-value products.
-select
+SELECT
 	hits_product_productsku,
 	hits_product_v2productname,
-	SUM(hits_product_productrevenue) / COUNT(hits_transaction_transactionid) as revenue_per_transaction
-from
+	SUM(hits_product_productrevenue) / COUNT(hits_transaction_transactionid) AS revenue_per_transaction
+FROM
 	all_transactions 
-where 
-	hits_product_productsku is not null
-group by
+WHERE 
+	hits_product_productsku IS NOT NULL
+GROUP BY
 	hits_product_productsku,
 	hits_product_v2productname
-order by 
-	revenue_per_transaction desc 
-limit 10;
+ORDER BY 
+	revenue_per_transaction DESC 
+LIMIT 10;
 
 -- ************************************************
 
 -- Query to find the top 10 products with the most transactions. 
 -- This could help identify popular products.
-select
+SELECT
 	hits_product_productsku,
 	hits_product_v2productname,
-	COUNT(hits_transaction_transactionid) as number_of_trasaction
-from
+	COUNT(hits_transaction_transactionid) AS number_of_transaction
+FROM
 	all_transactions 
-inner join
+INNER JOIN
 	product_list pl 
-on
+ON
 	all_transactions.hits_product_productsku = pl.productsku
-group by 
+GROUP BY 
 	hits_product_productsku,
 	hits_product_v2productname
-order by 
-	number_of_trasaction desc 
-limit 10;
+ORDER BY 
+	number_of_transaction DESC 
+LIMIT 10;
 
 -- ************************************************
 
 -- Query to find which cities have the highest average transaction revenue. 
 -- This could help target marketing efforts in regions with high spending power.
-select 
+SELECT 
 	geonetwork_city,
-	ROUND(AVG(totals_totaltransactionrevenue)) as avg_transaction_revenue
-from 
+	ROUND(AVG(totals_totaltransactionrevenue)) AS avg_transaction_revenue
+FROM 
 	all_transactions 
-group by
+GROUP BY
 	geonetwork_city
-having
+HAVING
 	COUNT(*) > 100
-order by 
-	avg_transaction_revenue desc 
-limit 10;
+ORDER BY 
+	avg_transaction_revenue DESC 
+LIMIT 10;
 
 -- ************************************************
 
 -- More complex query that uses window functions to find the top 3 products (by revenue) for each city. 
 -- This can provide insights into regional product preferences.
-select 
+SELECT 
 	city_product_rank.geonetwork_city,
 	city_product_rank.hits_product_productsku,
 	city_product_rank.total_revenue,
 	city_product_rank.product_rank
-from
+FROM
 	(
-		select
+		SELECT
 			geonetwork_city,
 			hits_product_productsku,
-			SUM(hits_product_productrevenue) as total_revenue,
-			RANK() over (
-				partition by geonetwork_city
-				order by SUM(hits_product_productrevenue) desc
-			) as product_rank
-		from 
+			SUM(hits_product_productrevenue) AS total_revenue,
+			RANK() OVER (
+				PARTITION BY geonetwork_city
+				ORDER BY SUM(hits_product_productrevenue) DESC
+			) AS product_rank
+		FROM 
 			all_transactions
-		where 
-			geonetwork_city is not null and geonetwork_city not in ('not available in demo dataset', '(not set)')
-		group by
+		WHERE 
+			geonetwork_city IS NOT NULL AND geonetwork_city NOT IN ('not available in demo dataset', '(not set)')
+		GROUP BY
 			geonetwork_city,
 			hits_product_productsku
-	) as city_product_rank
-where 
+	) AS city_product_rank
+WHERE 
 	city_product_rank.product_rank <= 3;
 
 -- ************************************************
